f3b276f8586cecd0359d591318b873a026aa688a
Daniela Malgapo <cryon.ixx911@gmail.com>
2025-12-12 19:45:02 +0800

    Added Nativewind + TailwindCSS

---
+++ src/app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      className="flex-1 items-center justify-center bg-blue-100"
+    >
+      <Text className="text-amber-800 items-center">Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
