de84d37af01c9a2ad01368f8b2efad0a72ad6c50
Daniela Malgapo <cryon.ixx911@gmail.com>
2026-01-14 04:25:33 +0800

    Revert "pushed all recent changes"
    This reverts commit 60b743db61d2597f937da5864730d75694b8c169.

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -5,7 +5,7 @@ export default function RootLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen
-        name="index"
+        name="Index"
         options={{
           title: "Welcome",
         }}
--- src/app/login/childlogin.tsx
+++ src/app/login/childlogin.tsx
@@ -1,52 +1,48 @@
-import { router } from "expo-router";
-import { Pressable, Text, View } from "react-native";
-import IndexReturn from "../../components/IndexReturn";
+import { router } from 'expo-router';
+import { Pressable, Text, View } from 'react-native';
+import IndexReturn from '../../components/IndexReturn';
 
 export default function ChildLogin() {
-  return (
-    <View className="flex-1 items-center justify-center bg-green-500">
-      <View className="w-full h-1/10 flex m-4">
-        <IndexReturn />
-      </View>
-      <View
-        className={[
-          "h-3/5",
-          "w-4/5",
-          "items-start",
-          "rounded-xl",
-          "bg-white",
-          "p-8",
-          "drop-shadow-lg",
-          "m-5",
-        ].join(" ")}
-      >
-        <View className="w-full">
-          <Text className="text-center text-xl text-green-500">
-            Hello! Tap your name to continue your adventure!
-          </Text>
-        </View>
+     return (
+          <View className="flex-1 items-center justify-center bg-green-500">
+               <View className="w-full h-1/10 flex m-4">
+                    <IndexReturn />
+               </View>
+               <View
+                    className={[
+                         'h-3/5',
+                         'w-4/5',
+                         'items-start',
+                         'rounded-xl',
+                         'bg-white',
+                         'p-8',
+                         'drop-shadow-lg',
+                         'm-5'
+                    ].join(' ')}
+               >
+                    <View className="w-full">
+                         <Text className="text-center text-xl text-green-500">Hello! Tap your name to continue your adventure!</Text>
+                    </View>
 
-        <View className="mt-4 w-full h-2/5 border-2 border-green-500 rounded-xl items-center justify-center">
-          <Text className="text-gray-950 text-lg font-bold">
-            • Child User 1
-          </Text>
-        </View>
+                         <View className="mt-4 w-full h-2/5 border-2 border-green-500 rounded-xl items-center justify-center">
+                              <Text className="text-gray-950 text-lg font-bold">• Child User 1</Text>
+                         </View>
 
-        <Pressable
-          onPress={() => router.push("/(child-tabs)/Home")}
-          className={[
-            "mt-4",
-            "w-full",
-            "items-center",
-            "rounded-xl",
-            "bg-yellow-400",
-          ].join(" ")}
-        >
-          <Text className={["p-4", "font-bold", "text-white"].join(" ")}>
-            Let's go!
-          </Text>
-        </Pressable>
-      </View>
-    </View>
-  );
+                    <Pressable  
+                        onPress={() => router.push('/(child-tabs)/Home')}
+                        className={[
+                              'mt-4',
+                              'w-full',
+                              'items-center',
+                              'rounded-xl',
+                              'bg-yellow-400',
+                         ].join(' ')}
+                    >
+                         <Text className={['p-4', 'font-bold', 'text-white'].join(' ')}>
+                              Let's go!
+                         </Text>
+                    </Pressable>
+               </View>
+          </View>
+     );
 }
